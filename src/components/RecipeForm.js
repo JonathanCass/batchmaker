96aa48e4fd77085d360eea3d96534e13f830ab73
JonCass <JCass1980@gmail.com>
2017-04-13 09:46:39 -0700

    moving on to database for real

--- src/components/RecipeForm.js
+++ src/components/RecipeForm.js
@@ -9,18 +9,57 @@ const styles ={
 	formProper:{
 		color: 'white',
 		width: 700,
-		height: 1000,
-		//background: '#006064',
-		background: 'linear-gradient( #006659, #004D40)',
+		height: 1220,
+		padding: 20,
+		background: 'black',
 		margin: '70px auto',
 		borderRadius: 20,
-		boxShadow: '15px 15px 10px 0px rgba(156,39,176,1)'
+		boxShadow: '15px 15px 10px 0px rgba(211,19,19,1)'
 	},
 	addPhoto:{
-		background:'pink',
+		background:'#D50000',
 		height: 190,
 		width: 190,
-		borderRadius: 20
+		borderRadius: 20,
+		textAlign: 'center',
+		fontSize: 20,
+		lineHeight: '188px',
+	},
+	recipeBy:{
+		width: 480,
+		height: 60,
+	},
+	recipeType:{
+		width: 220,
+		height: 50,
+	},
+	prepTimeTemp:{
+		width: 120,
+		height: 50,
+	},
+	degrees:{
+		width: 60,
+		height: 50
+	},
+	amount:{
+		width: 90,
+		height: 50
+	},
+	measurement:{
+		width: 410,
+		height: 50
+	},
+	saveRecipe:{
+		width: 250,
+		height: 60
+	},
+	infoHeader:{
+		width: 660,
+		border: '2px solid #009688',
+		borderWidth: ' 0 0 2px 0',
+		paddingBottom: 10,
+		fontSize: 24,
+		marginBottom: 10
 	}
 }
 class RecipeForm extends React.Component {
@@ -32,30 +71,30 @@ class RecipeForm extends React.Component {
     return (
       <div style={styles.formContainer}>
       	<div style={styles.formProper}>
-      		<form>Standard Recipe Information
-      			<img src="" alt="" style={styles.addPhoto}/>
-      			<input type="text" placeholder="Recipe Name"></input>
-      			<input type="text" placeholder="By"></input>
+      		<form><div style={styles.infoHeader}>Standard Recipe Information</div>
+      			<img src="" alt="ADD VISUAL" style={styles.addPhoto}/>
+      			<input type="text" style={styles.recipeBy} placeholder="Recipe Name"></input>
+      			<input type="text" style={styles.recipeBy} placeholder="By"></input>
       			<input type="radio" name="access" value="public"></input>Unrestricted Access
       			<input type="radio" name="access" value="private"></input>This User Access Only
-      			<select>
+      			<select style={styles.recipeType}>
 				  <option value="">Recipe Type</option>
 				  <option value="breakfast">Breakfast</option>
 				  <option value="lunch">Lunch</option>
 				  <option value="dinner">Dinner</option>
 				  <option value="dessert">Dessert</option>
 				</select>
-				<input type="text" placeholder="Prep Time"></input>
-				<input type="text" placeholder="Cook Time"></input>
-				<input type="text" placeholder="Cook Temp"></input>
-				<select>
+				<input type="text" style={styles.prepTimeTemp} placeholder="Prep Time"></input>
+				<input type="text" style={styles.prepTimeTemp} placeholder="Cook Time"></input>
+				<input type="text" style={styles.prepTimeTemp} placeholder="Cook Temp"></input>
+				<select style={styles.degrees}>
 					<option value="f">F°</option>
 					<option value="c">C°</option>
 				</select>
-				Recipe execution results in <input type="text" placeholder="Amount"></input>
-      			<input type="text" placeholder="Unit of measurement to be applied to result"></input>
+				Recipe execution results in <input type="text" style={styles.amount} placeholder="Amount"></input>
+      			<input type="text" style={styles.measurement} placeholder="Unit of measurement to be applied to result"></input>
       			<StepAdder/>
-      			<button>Input this Food Method</button>
+      			<button style={styles.saveRecipe}>Input this Food Method</button>
       		</form>
         </div>
       </div>
--- src/components/StepAdder.js
+++ src/components/StepAdder.js
@@ -3,20 +3,45 @@ import React from 'react'
 const styles={
 	stepContainer:{
 		color: 'white'
+	},
+	amount:{
+		width: 90,
+		height: 50
+	},
+	unit:{
+		width: 125,
+		height: 50
+	},
+	ingredient:{
+		width: 380,
+		height: 50
+	},
+	addIngredient:{
+		width: 60,
+		height: 50
+	},
+	directions:{
+		width: 700,
+		height: 150,
+		borderRadius: 5
+	},
+	addStep:{
+		width: 220,
+		height: 50
 	}
 }
 
 class StepAdder extends React.Component {
-  constructor(props) {
+  /*constructor(props) {
     super(props)
-  }
+  }*/
 
   render() {
     return (
       <div style={styles.stepContainer}>
         <form>
-        	<input type="text" placeholder="Amount"></input>
-        	<select>
+        	<input type="text" style={styles.amount} placeholder="Amount"></input>
+        	<select style={styles.unit}>
 				<option value="">Unit</option>
 				<option value="tsp">Tsp.</option>
 				<option value="tbsp">Tbsp.</option>
@@ -27,10 +52,10 @@ class StepAdder extends React.Component {
 				<option value="quart">Quart</option>
 				<option value="gallon">Gallon</option>
 			</select>
-			<input type="text" placeholder="Ingredient"></input>
-			<button>+</button>
-			<textarea placeholder="">Input procedure for this production phase. </textarea>
-        	<button>Add This Step</button>
+			<input type="text" style={styles.ingredient} placeholder="Ingredient"></input>
+			<button style={styles.addIngredient}>+</button>
+			<textarea style={styles.directions} placeholder="">Input procedure for this production phase. </textarea>
+        	<button style={styles.addStep}>Add This Step</button>
         </form>
       </div>
     )
